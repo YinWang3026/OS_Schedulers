c69cfd9eb9a364359b2f3cd49f1d4d7b80420f07
Yin Wang <yw2178@nyu.edu>
2021-03-12 15:09:59 -0500

    arg parse done

--- sched.cpp
+++ sched.cpp
@@ -3,9 +3,20 @@
 #include <string>
 #include <vector>
 #include <stdio.h>
+#include <getopt.h> //Arg parsing
+#include <stdlib.h>
 
 using namespace std;
 
+//Arguments
+int quantum = 10000; //RR, PRIO, PREPRIO, default = 100000
+int vFlag = 0;
+int tFlag = 0;
+int eFlag = 0;
+int maxPrio = 4; //PRIO, PREPRIO, default = 4
+char sched;
+
+//Object definitions
 enum process_state_t{ 
     STATE_CREATED,
     STATE_READY,
@@ -59,6 +70,9 @@ struct event{
     process_trans_t transition;
     event* next;
 };
+
+int process::count = 0; //Init the static count for PID
+int event::count = 0; //Init the static count for evt id
 
 class events{
     public:
@@ -120,20 +134,47 @@ class Scheduler {
 //Globals
 vector<int> randvals; //Vector containg random integers
 int rsize; //Total random integer count
-int quantum; //RR
 
 //Functions
 int myrandom(int);
 void printProcList(const vector<process*>&);
 void deleteProcList(const vector<process*>&);
-void Simulation(events*);
-
-int process::count = 0; //Init the static count for PID
-int event::count = 0; //Init the static count for evt id
+void simulation(events&);
 
 int main(int argc, char* argv[]) {
+
+    int c;
+    while ((c = getopt(argc,argv,"vtes:")) != -1 )
+    {
+        switch(c) {
+        case 'v': 
+            vFlag = 1;
+            break;
+        case 't': 
+            tFlag = 1;
+            break;
+        case 'e':
+            eFlag = 1;
+            break;
+        case 's':
+            sscanf(optarg, "%c%d:%d",&sched,&quantum,&maxPrio);
+            break;
+        }
+    }
+
+    printf("vflag = %d  tflag = %d eflag = %d\n",vFlag,tFlag,eFlag); 
+    printf("Scheduler: %c, quantum: %d, maxPrio: %d\n",sched,quantum,maxPrio);
+
+    if ((argc - optind) < 2) { //optind is the index of current argument
+        cerr << "Missing input file and rfile\n";
+        exit(1);
+    }
+    char* inputFile = argv[optind];
+    char* rFile = argv[optind+1];
+    cout << inputFile << "\t" << rFile << endl;
+    
     //Opening random value file
-    ifstream rfile("../lab2_assign/rfile");
+    ifstream rfile(rFile);
     if (!rfile) {
         cerr << "Could not open the rfile.\n";
         exit(1);
@@ -146,7 +187,7 @@ int main(int argc, char* argv[]) {
     rfile.close();
     
     //Opening process file
-    ifstream ifile("../lab2_assign/input0");
+    ifstream ifile(inputFile);
     if (!ifile) {
         cerr << "Could not open the input file.\n";
         exit(1);
@@ -159,13 +200,12 @@ int main(int argc, char* argv[]) {
 
     while (ifile >> at >> tc >> cb >> io) {
         p = new process(at,tc,cb,io,STATE_CREATED);
-        evt = new event(p, at, TRANS_TO_PREEMPT);
+        evt = new event(p, at, TRANS_TO_READY);
         procList.push_back(p);
         evtList.putEvent(evt);
     }
     ifile.close(); //Closing file
 
-    /*
     printProcList(procList); //Print the process list
     evtList.printEvents(); //Print the event list
     event* temp; //Checking if getEvent works
@@ -175,9 +215,8 @@ int main(int argc, char* argv[]) {
     }
     evtList.rmEvent(evt); //Remove event test
     evtList.printEvents(); //Print the event list
-    */
-
-    Simulation(&evtList);
+    
+    simulation(evtList);
     deleteProcList(procList); //Deleting allocated memory
 }
 
@@ -205,48 +244,53 @@ void deleteProcList(const vector<process*>& v){
     }
 }
 
-void Simulation(events* evtList){
-    return ;
-}
-/*
-void Simulation() {
-EVENT* evt;
-while( (evt = get_event()) ) {
-Process *proc = evt->evtProcess;    // this is the process the event works on
-CURRENT_TIME = evt->evtTimeStamp;
-timeInPrevState = CURRENT_TIME – proc->state_ts;
-switch(evt->transition) {  // which state to transition to?
-case TRANS_TO_READY:
-// must come from BLOCKED or from PREEMPTION
-// must add to run queue
-CALL_SCHEDULER = true; // conditional on whether something is run
-break;
-case TRANS_TO_RUN:
-// create event for either preemption or blocking
-break;
-case TRANS_TO_BLOCK:
-//create an event for when process becomes READY again
-CALL_SCHEDULER = true;
-break;
-case TRANS_TO_PREEMPT:
-// add to runqueue (no event is generated)
-CALL_SCHEDULER = true;
-break;
-}
-// remove current event object from Memory
-delete evt; evt = nullptr;
-if(CALL_SCHEDULER) {
-if (get_next_event_time() == CURRENT_TIME)
-continue; //process next event from Event queue
-CALL_SCHEDULER = false; // reset global flag
-if (CURRENT_RUNNING_PROCESS == nullptr) {
-CURRENT_RUNNING_PROCESS = THE_SCHEDULER->get_next_process();
-if (CURRENT_RUNNING_PROCESS == nullptr)
-continue;
+void simulation(events& evtList){
+    event* evt;
+    process* proc;
+    process_trans_t evtTrans;
+    int currentTime, timeInPrevState;
+    while((evt = evtList.getEvent())) {
+        //Get the details
+        proc = evt->evtProcess;
+        evtTrans = evt->transition;
+        currentTime = evt->evtTimeStamp;
+        timeInPrevState = currentTime - proc->stateTimeStamp;
+        //Delete the evt
+        delete evt;
+        evt = nullptr;
+        /*
+        switch(evtTrans) {  // which state to transition to?
+            case TRANS_TO_READY:
+                // must come from BLOCKED or from PREEMPTION
+                // must add to run queue
+                CALL_SCHEDULER = true; // conditional on whether something is run
+                break;
+            case TRANS_TO_RUN:
+                // create event for either preemption or blocking
+                break;
+            case TRANS_TO_BLOCK:
+                //create an event for when process becomes READY again
+                CALL_SCHEDULER = true;
+                break;
+            case TRANS_TO_PREEMPT:
+                // add to runqueue (no event is generated)
+                CALL_SCHEDULER = true;
+                break;
+        }
+        // remove current event object from Memory
 
-}}}}
- */
-
+        if(CALL_SCHEDULER) {
+            if (get_next_event_time() == CURRENT_TIME)
+                continue; //process next event from Event queue
+            CALL_SCHEDULER = false; // reset global flag
+            if (CURRENT_RUNNING_PROCESS == nullptr) {
+                CURRENT_RUNNING_PROCESS = THE_SCHEDULER->get_next_process();
+                if (CURRENT_RUNNING_PROCESS == nullptr)
+                    continue;
+            }
+        } */
+    }
+}
 // create event to make this process runnable for same time.
 
 /*
