d3500c77e2cebbe2c3028de9bc8e7c09229a6064
Yin Wang <yw2178@nyu.edu>
2021-03-12 16:47:16 -0500

    flags

--- sched.cpp
+++ sched.cpp
@@ -8,20 +8,32 @@
 
 using namespace std;
 
-//Arguments
-int quantum = 10000; //RR, PRIO, PREPRIO, default = 100000
+//Globals
 int vFlag = 0;
 int tFlag = 0;
 int eFlag = 0;
-int maxPrio = 4; //PRIO, PREPRIO, default = 4
-char sched;
 
-//Object definitions
+vector<int> randvals; //Vector containg random integers
+int rsize; //Total random integer count
+
+//Macro definitions
+#define vtrace(fmt...)  do { if (vFlag) { printf(fmt); fflush(stdout); } } while(0)
+#define ttrace(fmt...)  do { if (tFlag) { printf(fmt); fflush(stdout); } } while(0)
+#define etrace(fmt...)  do { if (eFlag) { printf(fmt); fflush(stdout); } } while(0)
+
+//Enum definitions
 enum process_state_t{ 
     STATE_CREATED,
     STATE_READY,
     STATE_RUNNING,
     STATE_BLOCKED
+};
+
+string enum_state_string[] = { //Print the state
+    "CREAETD",
+    "READY",
+    "RUNNING",
+    "BLOCKED"
 };
 
 enum process_trans_t{
@@ -31,6 +43,7 @@ enum process_trans_t{
     TRANS_TO_PREEMPT //Create -> Ready
 };
 
+//Struct definitions
 struct process{
     static int count;
     process(int at, int ct, int cb, int io, process_state_t s):
@@ -41,15 +54,17 @@ struct process{
         printf("PID: %d, AT: %d, TC: %d, CB: %d, IO: %d\n",pid,arrivalTime,totalCpuTime,
         cpuBurst,ioBurst);
     }
+    int pid; //process id
     int arrivalTime;
     int totalCpuTime;
     int cpuBurst;
     int ioBurst;
-    process_state_t state;
-    int remainCpuTime;
-    int staticPriority;
-    int stateTimeStamp;
-    int pid;
+    process_state_t state; //current state
+    //To be modified
+    int remain_cputime;
+    int static_priority;
+    int dynamic_priority; //static prio - 1
+    int state_ts; 
 };
 
 struct event{
@@ -64,22 +79,27 @@ struct event{
         printf("evtID: %d, PID: %d, ts: %d, trans: %d\n",evtid,evtProcess->pid,evtTimeStamp,
         transition);
     }
-    int evtid;
-    process* evtProcess;
-    int evtTimeStamp;
-    process_trans_t transition;
-    event* next;
+    void eTraceEvent(){
+        cout << evtTimeStamp << ":" << evtProcess->pid << ":" << enum_state_string[evtProcess->state] << " ";
+        //etrace("%d:%d:%s ",evtTimeStamp,evtProcess->pid,enum_state_string[evtProcess->state].c_str());
+    }
+    int evtid; //An id
+    process* evtProcess; //The proc associated with this event
+    int evtTimeStamp; //Time to fire this event
+    process_trans_t transition; //Which state to bring the proc to
+    event* next; //Pointer to next
 };
 
-int process::count = 0; //Init the static count for PID
-int event::count = 0; //Init the static count for evt id
+int process::count = 0; //Init the static count for pid
+int event::count = 0; //Init the static count for evtid
 
-class events{
+//Class definitions
+class Events{
     public:
-        events(){
+        Events(){
             head = new event();
         }
-        ~events(){
+        ~Events(){
             delete head;
         }
         //Return the first evt in list
@@ -92,13 +112,18 @@ class events{
             head->next = head->next->next;
             return temp;
         }
-        //Add to the end of the list
+        //Add to the end of the list based on its timestamp
         void putEvent(event* evt){
-            event* temp = head;
-            while (temp->next != NULL){
-                temp = temp->next;
+            event* tempEvt = head->next;
+            event* prevEvt = head;
+            while (tempEvt != NULL){
+                if (evt->evtTimeStamp < tempEvt->evtTimeStamp){
+                    break;
+                }
+                prevEvt = prevEvt->next;
+                tempEvt = tempEvt->next;
             }
-            temp->next = evt;
+            prevEvt->next = evt;
         }
         //Find evt by ID and remove it
         void rmEvent(event* evt){
@@ -119,31 +144,74 @@ class events{
             while (temp != NULL) {
                 temp->printEvent();
                 temp = temp->next;
-            }
+            }  
         }
 
     private:
         event* head; //Dummy head of linked list of evts
+        //Time stamp increases down the list
 };
 
 class Scheduler {
     public:
+        Scheduler(){}
+        virtual void add_process(process* p){
+            
+        }
+        virtual process* get_next_process(){
+            
+        }
+        virtual void test_preempty(process *p, int curtime){
+            
+        }
+    private:
+
+        //Run queue how i do this
+};
+
+class FCFS : public Scheduler{
+    public:
     private:
 };
 
-//Globals
-vector<int> randvals; //Vector containg random integers
-int rsize; //Total random integer count
+class LCFS : public Scheduler{
+    public:
+    private:
+};
 
-//Functions
-int myrandom(int);
-void printProcList(const vector<process*>&);
-void deleteProcList(const vector<process*>&);
-void simulation(events&);
+class SRTF : public Scheduler{
+    public:
+    private:
+};
+
+class RR : public Scheduler{
+    public:
+    private:
+};
+
+class PRIO : public Scheduler{
+    public:
+    private:
+};
+
+class PREPRIO : public Scheduler{
+    public:
+    private:
+};
+
+//Function prototypes
+void printProcList(vector<process*>&);
+void deleteProcList(vector<process*>&);
+int myrandom(int); //Generates a random number
+void simulation(Events&, Scheduler&);
+
 
 int main(int argc, char* argv[]) {
-
     int c;
+    int maxPrio = 4; //PRIO, PREPRIO, default = 4
+    int quantum = 10000; //RR, PRIO, PREPRIO, default = 100000
+    char sched; //Scheduler type
+    Scheduler myScheduler; //Scheduler for the simulator
     while ((c = getopt(argc,argv,"vtes:")) != -1 )
     {
         switch(c) {
@@ -159,11 +227,11 @@ int main(int argc, char* argv[]) {
         case 's':
             sscanf(optarg, "%c%d:%d",&sched,&quantum,&maxPrio);
             break;
+            ///ARGUEMNT CHECKING
         }
     }
-
-    printf("vflag = %d  tflag = %d eflag = %d\n",vFlag,tFlag,eFlag); 
-    printf("Scheduler: %c, quantum: %d, maxPrio: %d\n",sched,quantum,maxPrio);
+    vtrace("vflag = %d  tflag = %d eflag = %d\n",vFlag,tFlag,eFlag);
+    vtrace("Scheduler: %c, quantum: %d, maxPrio: %d\n",sched,quantum,maxPrio);
 
     if ((argc - optind) < 2) { //optind is the index of current argument
         cerr << "Missing input file and rfile\n";
@@ -171,7 +239,7 @@ int main(int argc, char* argv[]) {
     }
     char* inputFile = argv[optind];
     char* rFile = argv[optind+1];
-    cout << inputFile << "\t" << rFile << endl;
+    vtrace("Input file: %s, rfile: %s\n",inputFile,rFile);
     
     //Opening random value file
     ifstream rfile(rFile);
@@ -196,11 +264,12 @@ int main(int argc, char* argv[]) {
     event* evt;
     int at, tc, cb, io;
     vector<process*> procList; //Vector holding all created procs
-    events evtList; //Linked list holding all events
+    Events evtList; //Linked list holding all events
 
     while (ifile >> at >> tc >> cb >> io) {
         p = new process(at,tc,cb,io,STATE_CREATED);
         evt = new event(p, at, TRANS_TO_READY);
+        //MAX_PRIO????????????????????
         procList.push_back(p);
         evtList.putEvent(evt);
     }
@@ -216,7 +285,7 @@ int main(int argc, char* argv[]) {
     evtList.rmEvent(evt); //Remove event test
     evtList.printEvents(); //Print the event list
     
-    simulation(evtList);
+    simulation(evtList, myScheduler);
     deleteProcList(procList); //Deleting allocated memory
 }
 
@@ -231,20 +300,20 @@ int myrandom(int burst) {
 }
 
 //Prints the vector of procs
-void printProcList(const vector<process*>& v){
+void printProcList(vector<process*>& v){
     for (int i=0; i<v.size(); i++){
         v[i]->printProcess();
     }
 }
 
 //Deleting every allocated procs
-void deleteProcList(const vector<process*>& v){
+void deleteProcList(vector<process*>& v){
     for (int i=0; i<v.size(); i++){
         delete v[i];
     }
 }
 
-void simulation(events& evtList){
+void simulation(Events& evtList, Scheduler& myScheduler){
     event* evt;
     process* proc;
     process_trans_t evtTrans;
@@ -254,7 +323,7 @@ void simulation(events& evtList){
         proc = evt->evtProcess;
         evtTrans = evt->transition;
         currentTime = evt->evtTimeStamp;
-        timeInPrevState = currentTime - proc->stateTimeStamp;
+        timeInPrevState = currentTime - proc->state_ts;
         //Delete the evt
         delete evt;
         evt = nullptr;
